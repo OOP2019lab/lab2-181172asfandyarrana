d3ab1c68aa8f26cfe0e14d1e99d12056cd8f7411
181172asfandyarrana <46881446+181172asfandyarrana@users.noreply.github.com>
2019-01-28 11:00:00 +0500

    Add files via upload

---
+++ q2_181172.h
@@ -0,0 +1,13 @@
+#include<string>
+using namespace std;
+struct githubuser
+{
+	string firstName;
+	string userName;
+	string password;
+	string email; 
+    int folderCount;
+	string *gitHubFolders;
+
+
+};
